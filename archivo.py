f8fe305daf941e9e7b610a0af013dd3652cbb471
Estuardo Gutiérrez <EAGUTIERREZTZ@correo.url.edu.gt>
2022-03-15 09:27:14 -0600

    Se ha creado el archivo pyhton
    Se ha integrado el archivo de python.

---
+++ archivo.py
@@ -0,0 +1,8 @@
+Notamate1 = float(input("Porfavor ingrese su nota en matemática 1: "))
+Notaquim1 = float(input("Porfavor ingrese su nota en quimica 1: "))
+Notaintroaprogra = float(input("Porfavor ingrese su nota en introducción a la programación:"))
+Notacomunicacion = float(input("Porfavor ingrese su nota en comunicación linguistica: "))
+Notaintroainge = float (input("Porfavor ingrese su nota en introducción a la ingenieria en sistemas: "))
+Notaestrategias = float(input("Porfavor ingrese su nota en estrategias de razonamiento: "))
+Promedio = float((Notamate1 + Notaquim1 + Notaintroaprogra + Notacomunicacion + Notaintroainge + Notaestrategias)/6)
+print("Su promedio es de: ",Promedio)
